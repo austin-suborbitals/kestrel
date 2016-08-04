1102ff598bde959a5ff3f28411b011757567a16b
Zach Marcantel <zmarcantel@gmail.com>
2016-08-04 01:14:37 -0500

    POC using kinetis and k64 cfg

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,19 @@
 #![feature(lang_items)]
 #![crate_type="staticlib"]
 
-
 //------------------------------------------------
 //
 // modules
 //
 //------------------------------------------------
 
+#![cfg(vendor = "kinetis")]
+#[cfg(mcu = "k64")]
+
 extern crate peregrine;
+use peregrine::mcus::kinetis::k64;
 
 pub mod except;
+
+
+static WDOG: k64::Watchdog = k64::Watchdog(0x1000 as *const u8);
