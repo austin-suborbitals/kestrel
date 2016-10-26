d08b445cef8dd0c20f393830c761956718a3779d
Travis-Generated Documentation <kestrel@travis-ci.org>
2016-10-26 23:17:16 +0000

    doc upload for kestrel (c4f41205b8142c76e11b77f1773b7ba0d88fbb86)

--- implementors/core/iter/iterator/trait.Iterator.js
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["bitmap"] = ["impl&lt;'a,&nbsp;S:&nbsp;<a class='trait' href='bitmap/trait.Storage.html' title='bitmap::Storage'>Storage</a>,&nbsp;E:&nbsp;<a class='trait' href='bitmap/trait.Element.html' title='bitmap::Element'>Element</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='bitmap/struct.Slices.html' title='bitmap::Slices'>Slices</a>&lt;'a,&nbsp;S,&nbsp;E&gt;",];
+implementors["bitmap"] = ["impl&lt;'a, S:&nbsp;<a class='trait' href='bitmap/trait.Storage.html' title='bitmap::Storage'>Storage</a>, E:&nbsp;<a class='trait' href='bitmap/trait.Element.html' title='bitmap::Element'>Element</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='bitmap/struct.Slices.html' title='bitmap::Slices'>Slices</a>&lt;'a, S, E&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/iter/traits/trait.IntoIterator.js
+++ implementors/core/iter/traits/trait.IntoIterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["bitmap"] = ["impl&lt;'a,&nbsp;S:&nbsp;<a class='trait' href='bitmap/trait.Storage.html' title='bitmap::Storage'>Storage</a>,&nbsp;E:&nbsp;<a class='trait' href='bitmap/trait.Element.html' title='bitmap::Element'>Element</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='bitmap/struct.Bitmap.html' title='bitmap::Bitmap'>Bitmap</a>&lt;S,&nbsp;E&gt;",];
+implementors["bitmap"] = ["impl&lt;'a, S:&nbsp;<a class='trait' href='bitmap/trait.Storage.html' title='bitmap::Storage'>Storage</a>, E:&nbsp;<a class='trait' href='bitmap/trait.Element.html' title='bitmap::Element'>Element</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html' title='core::iter::traits::IntoIterator'>IntoIterator</a> for &amp;'a <a class='struct' href='bitmap/struct.Bitmap.html' title='bitmap::Bitmap'>Bitmap</a>&lt;S, E&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
